944aec94e044f816b824d464eba1b436fe5bc8ef
sulaimondawood <sulaimondawod@gmail.com>
2025-09-09 12:12:47 +0100

    added user model

---
+++ src/main/java/com/dawood/finance/dtos/auth/RegisterRequestDTO.java
@@ -0,0 +1,5 @@
+package com.dawood.finance.dtos.auth;
+
+public class RegisterRequestDTO {
+
+}
---
+++ src/main/java/com/dawood/finance/entities/User.java
@@ -0,0 +1,46 @@
+package com.dawood.finance.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String fullname;
+
+  @Column(unique = true)
+  private String email;
+
+  private String password;
+
+  private String photoUrl;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
+
+}
