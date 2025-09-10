5ad0ce4abb1e5c6f271b57b8587be50a04bb078f
sulaimondawood <sulaimondawod@gmail.com>
2025-09-10 23:02:16 +0100

    implemented spring config

--- src/main/java/com/dawood/finance/configs/AppConfig.java
+++ src/main/java/com/dawood/finance/configs/AppConfig.java
@@ -1,16 +1,26 @@
 package com.dawood.finance.configs;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.dawood.finance.dtos.auth.ErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -26,6 +36,9 @@ public class AppConfig {
         .authorizeHttpRequests((request) -> request.requestMatchers("api/v1/auth/**").permitAll()
             .anyRequest().authenticated())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .exceptionHandling(exception -> exception
+            .authenticationEntryPoint(getAuthenticationEntryPoint())
+            .accessDeniedHandler(getAccessDeniedHandler()))
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
@@ -40,6 +53,34 @@ public class AppConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
     return configuration.getAuthenticationManager();
+  }
+
+  private AuthenticationEntryPoint getAuthenticationEntryPoint() {
+    return (request, response, authException) -> {
+      writeErrorResponse(response, "Authentication failed", HttpStatus.UNAUTHORIZED.value());
+    };
+  }
+
+  private AccessDeniedHandler getAccessDeniedHandler() {
+    return (request, response, auth) -> {
+      writeErrorResponse(response, "User unauthorized", HttpStatus.FORBIDDEN.value());
+    };
+  }
+
+  private void writeErrorResponse(HttpServletResponse response, String message, int status)
+      throws JsonProcessingException, IOException {
+
+    response.setContentType("application/json");
+    response.setStatus(status);
+
+    ErrorResponse error = new ErrorResponse();
+
+    error.setStatus(status);
+    error.setMessage(message);
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    response.getWriter().write(mapper.writeValueAsString(error));
   }
 
 }
