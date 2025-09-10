8f3ed2731237b6a185320f0ade435cc89b7169c5
sulaimondawood <sulaimondawod@gmail.com>
2025-09-10 22:43:13 +0100

    finished app config

--- src/main/java/com/dawood/finance/configs/AppConfig.java
+++ src/main/java/com/dawood/finance/configs/AppConfig.java
@@ -2,10 +2,14 @@ package com.dawood.finance.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,17 +17,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AppConfig {
 
+  private final JwtFilter jwtFilter;
+
   @Bean
-public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-  httpSecurity.csrf((csrf)->csrf.disable())
-  
+    httpSecurity.csrf((csrf) -> csrf.disable())
+        .authorizeHttpRequests((request) -> request.requestMatchers("api/v1/auth/**").permitAll()
+            .anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-}
+    return httpSecurity.build();
+
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
   }
 
 }
