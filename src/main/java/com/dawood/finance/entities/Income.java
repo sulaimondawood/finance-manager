9009bba7f5b227b0e5ec23d4dc587e85884764ae
sulaimondawood <sulaimondawod@gmail.com>
2025-09-14 21:41:07 +0100

    added expense and income entities

---
+++ src/main/java/com/dawood/finance/entities/Income.java
@@ -0,0 +1,65 @@
+package com.dawood.finance.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "incomes")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Income {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String name;
+
+  private BigDecimal amount;
+
+  private LocalDate date;
+
+  private String icon;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  public void prepersit() {
+    if (date == null)
+      this.date = LocalDate.now();
+  }
+
+}
