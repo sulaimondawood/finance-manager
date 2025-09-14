104a73971d6e0b0f8923c7e5b9c0da09a28a196b
sulaimondawood <sulaimondawod@gmail.com>
2025-09-14 23:17:56 +0100

    added transaction mapper

---
+++ src/main/java/com/dawood/finance/mappers/TransactionMapper.java
@@ -0,0 +1,34 @@
+package com.dawood.finance.mappers;
+
+import com.dawood.finance.dtos.transaction.ExpenseResponseDTO;
+import com.dawood.finance.dtos.transaction.IncomeResponseDTO;
+import com.dawood.finance.entities.Expense;
+import com.dawood.finance.entities.Income;
+
+public class TransactionMapper {
+
+  public ExpenseResponseDTO toExpenseDTO(Expense expense) {
+    return ExpenseResponseDTO.builder()
+        .id(expense.getId())
+        .name(expense.getName())
+        .amount(expense.getAmount())
+        .date(expense.getDate())
+        .icon(expense.getIcon())
+        .updatedAt(expense.getUpdatedAt())
+        .createdAt(expense.getCreatedAt())
+        .build();
+  }
+
+  public IncomeResponseDTO toIncomeDTO(Income income) {
+    return IncomeResponseDTO.builder()
+        .id(income.getId())
+        .name(income.getName())
+        .amount(income.getAmount())
+        .date(income.getDate())
+        .icon(income.getIcon())
+        .updatedAt(income.getUpdatedAt())
+        .createdAt(income.getCreatedAt())
+        .build();
+  }
+
+}
--- src/main/java/com/dawood/finance/services/TransactionService.java
+++ src/main/java/com/dawood/finance/services/TransactionService.java
@@ -3,8 +3,16 @@ package com.dawood.finance.services;
 import org.springframework.stereotype.Service;
 
 import com.dawood.finance.dtos.ApiResponse;
+import com.dawood.finance.dtos.transaction.ExpenseRequestDTO;
+import com.dawood.finance.dtos.transaction.ExpenseResponseDTO;
+import com.dawood.finance.entities.Category;
+import com.dawood.finance.entities.Expense;
+import com.dawood.finance.entities.User;
+import com.dawood.finance.exceptions.category.CategoryNotFoundException;
+import com.dawood.finance.repositories.CategoryRepository;
 import com.dawood.finance.repositories.ExpenseRepository;
 import com.dawood.finance.repositories.IncomeRepository;
+import com.dawood.finance.services.auth.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +24,28 @@ public class TransactionService {
 
   private final IncomeRepository incomeRepository;
 
-  // public ApiResponse
+  private final CategoryRepository categoryRepository;
+
+  private final AuthService authService;
+
+  public ApiResponse<ExpenseResponseDTO> addExpense(ExpenseRequestDTO request) {
+
+    Category category = categoryRepository.findById(request.getCategoryId())
+        .orElseThrow(() -> new CategoryNotFoundException());
+
+    User user = authService.getCurrentUser();
+
+    Expense newExpense = new Expense();
+
+    newExpense.setName(request.getName());
+    newExpense.setAmount(request.getAmount());
+    newExpense.setDate(request.getDate());
+    newExpense.setIcon(request.getIcon());
+    newExpense.setUser(user);
+    newExpense.setCategory(category);
+
+    return null;
+
+  }
 
 }
