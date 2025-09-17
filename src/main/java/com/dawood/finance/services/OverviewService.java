b7b67e24710bf445834e58836c3122d331263230
sulaimondawood <sulaimondawod@gmail.com>
2025-09-17 16:06:44 +0100

    added notification service

--- src/main/java/com/dawood/finance/services/OverviewService.java
+++ src/main/java/com/dawood/finance/services/OverviewService.java
@@ -86,4 +86,59 @@ public class OverviewService {
 
   }
 
+  public Map<String, Object> getTransactionOverview(User user) {
+
+    Map<String, Object> overiew = new LinkedHashMap<>();
+
+    BigDecimal totalIncome = incomeRepository.findSumOfIncomesByUserId(user.getId());
+    totalIncome = (totalIncome != null) ? totalIncome : BigDecimal.ZERO;
+
+    List<IncomeResponseDTO> recentIncomes = incomeRepository.findTop5ByUserOrderByCreatedAtDesc(user).stream()
+        .map(TransactionMapper::toIncomeDTO).toList();
+
+    BigDecimal totalExpense = expenseRepository.findSumOfExpensesByUserId(user.getId());
+    if (totalExpense == null)
+      totalExpense = BigDecimal.ZERO;
+
+    List<ExpenseResponseDTO> recentExpenses = expenseRepository.findTop5ByUserOrderByCreatedAtDesc(user).stream()
+        .map(TransactionMapper::toExpenseDTO).toList();
+
+    BigDecimal totalBalance = totalIncome.subtract(totalExpense);
+
+    List<TransactionResponseDTO> allTransactions = Stream
+        .concat(incomeRepository.findTop5ByUserOrderByCreatedAtDesc(user).stream()
+            .map(income -> {
+              return TransactionResponseDTO.builder()
+                  .id(income.getId())
+                  .name(income.getName())
+                  .type("income")
+                  .amount(income.getAmount())
+                  .date(income.getDate())
+                  .icon(income.getIcon())
+                  .build();
+            }), expenseRepository.findTop5ByUserOrderByCreatedAtDesc(user).stream()
+                .map(expense -> {
+                  return TransactionResponseDTO.builder()
+                      .id(expense.getId())
+                      .name(expense.getName())
+                      .type("expense")
+                      .amount(expense.getAmount())
+                      .date(expense.getDate())
+                      .icon(expense.getIcon())
+                      .build();
+                }))
+        .sorted((t1, t2) -> t2.getDate().compareTo(t1.getDate()))
+        .toList();
+
+    overiew.put("totalBalance", totalBalance);
+    overiew.put("totalIncome", totalIncome);
+    overiew.put("totalExpense", totalExpense);
+    overiew.put("recentIncomes", recentIncomes);
+    overiew.put("recentExpenses", recentExpenses);
+    overiew.put("transactions", allTransactions);
+
+    return overiew;
+
+  }
+
 }
